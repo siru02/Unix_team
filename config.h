ba77d984a31e039542579bfb2b94438d23ebf0dd
siru02 <khm32323@gmail.com>
2024-12-14 19:47:57 +0900

    [feat]: 서버파일받아오기 성공

---
+++ config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define ROWS 128
+#define COLS 128
+#define NUM_CLIENTS 8  // 클라이언트 수
+
+#define OFFSET 8
+#define OFFSET_8 8 // 8 by 8
+#define OFFSET_4 4 // 4 by 4
+
+
+
+#endif
+
